171f48069386bdcd927924388cbb3ba16df10fe6
Oliver Lin <jumpy12359@gmail.com>
2016-10-11 21:40:35 +1100

    Adding a new gamemode to the country guessing game

--- guesscountry.py
+++ guesscountry.py
@@ -49,3 +49,27 @@ game2_population = ['China ', 'India ', 'United States ', 'Indonesia ', 'Brazil 
                     'Uzbekistan ', 'Saudi Arabia ', 'Yemen ', 'Ghana ', 'Mozambique ', 'North Korea']
 
 print("How many")
+
+# Game 3 United States Capital Cities
+# http://www.infoplease.com/ipa/A0763765.html
+game3_states = ["Alabama", "Alaska", "Arizona", "Arkansas", "California",
+                "Colorado", "Connecticut", "Delaware", "Florida", "Georgia",
+                "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa",
+                "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland",
+                "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri",
+                "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey",
+                "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio",
+                "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina",
+                "South Dakota", "Tennessee", "Texas", "Utah", "Vermont",
+                "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"]
+
+game3_capitals = ["Montgomery", "Juneau", "Phoenix", "Little Rock", "Sacramento",
+                  "Denver", "Hartford", "Dover", "Tallahassee", "Atlanta",
+                  "Honolulu", "Boise", "Springfield", "Indianapolis", "Des Moines",
+                  "Topeka", "Frankfort", "Baton Rouge", "Augusta", "Annapolis",
+                  "Boston", "Lansing", "St. Paul", "Jackson", "Jefferson City",
+                  "Helena", "Lincoln", "Carson City", "Concord", "Trenton",
+                  "Santa Fe", "Albany", "Raleigh", "Bismarck", "Columbus",
+                  "Oklahoma City", "Salem", "Harrisburg", "Providence", "Columbia",
+                  "Pierre", "Nashville", "Austin", "Salt Lake City", "Montpelier",
+                  "Richmond", "Olympia", "Charleston", "Madison", "Cheyenne"]
